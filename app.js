1d9a0d552f051d06375ea2987b8e6ded03838878
Ashwanikumar1715 <ashwanikumar01011997123@gmail.com>
2023-01-31 16:25:52 +0530

    pop balloon

---
+++ app.js
@@ -0,0 +1,29 @@
+let popped = 0;
+
+document.addEventListener('click', function(e){
+    
+    if (e.target.className === "balloon"){
+        
+                e.target.style.backgroundColor = "#ededed";
+                e.target.textContent = "POP!";
+                popped++;
+                removeEvent(e);
+                checkAllPopped();
+    }   
+});
+
+function removeEvent(e){
+    e.target.removeEventListener('click', function(){
+        
+    })
+};
+
+function checkAllPopped(){
+    if (popped === 25){
+        console.log('all popped!');
+        let gallery = document.querySelector('#balloon-gallery');
+        let message = document.querySelector('#yay-no-balloons');
+        gallery.innerHTML = '';
+        message.style.display = 'block';
+    }
+};
